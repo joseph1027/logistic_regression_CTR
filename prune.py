a8bd3e1089477d335413e58f413efe3b2c07e3b6
joseph1027 <joseph102712080104@gmail.com>
2018-11-05 11:05:32 +0800

    Update prune.py

--- prune.py
+++ prune.py
@@ -28,7 +28,7 @@ with open('/data2/prune/test.txt') as k:
 
 
 with open('/data2/prune/train.txt') as f:
-	with open('/data2/prune/train_out500.txt','a') as w:
+	with open('/data2/prune/train_out100.txt','a') as w:
 		for line in f:
 			one_row_data_arr = line.split()
 			w.write(one_row_data_arr[0]+' '+one_row_data_arr[1])
@@ -36,13 +36,13 @@ with open('/data2/prune/train.txt') as f:
 			for j in range(2,len(one_row_data_arr)):
 				a = one_row_data_arr[j].split(":")
 				feature_id = int(a[0])
-				if(count[feature_id]>=500):
+				if(count[feature_id]>=100):
 					w.write(' '+str(feature_id)+':1')
 				#count[feature_id] +=1
 			w.write('\n')
 
 with open('/data2/prune/test.txt') as f:
-	with open('/data2/prune/test_out500.txt','a') as w:
+	with open('/data2/prune/test_out100.txt','a') as w:
 		for line in f:
 			one_row_data_arr = line.split()
 			w.write(one_row_data_arr[0]+' '+one_row_data_arr[1])
@@ -50,7 +50,7 @@ with open('/data2/prune/test.txt') as f:
 			for j in range(2,len(one_row_data_arr)):
 				a = one_row_data_arr[j].split(":")
 				feature_id = int(a[0])
-				if(count[feature_id]>=500):
+				if(count[feature_id]>=100):
 					w.write(' '+str(feature_id)+':1')
 				#count[feature_id] +=1
 			w.write('\n')
